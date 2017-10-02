4fb9f44ca96c5007d9409ed8021b73587cf24e97
lighttiger <lighttiger2505@gmail.com>
2017-10-03 07:47:28 +0900

    Add implement browse command

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"runtime"
+	"strings"
 
 	"github.com/mitchellh/cli"
+)
+
+const (
+	ExitCodeOK        int = iota //0
+	ExitCodeError     int = iota //1
+	ExitCodeFileError int = iota //2
 )
 
 type BrowseCommand struct {
@@ -24,7 +35,141 @@ func (c *BrowseCommand) Run(args []string) int {
 
 	flags := flag.NewFlagSet("add", flag.ContinueOnError)
 	flags.BoolVar(&debug, "debug", false, "Run as DEBUG mode")
-	return 0
+
+	// Get remote repositorys
+	remotes := gitOutputs("git", []string{"remote"})
+
+	// Remote repository is not registered
+	if len(remotes) == 0 {
+		fmt.Println("No remote setting in this repository")
+		return ExitCodeError
+	}
+
+	remote := ""
+	for _, tmp_remote := range remotes {
+		if "origin" == tmp_remote {
+			remote = tmp_remote
+		}
+	}
+
+	var url string
+	if remote != "" {
+		url = gitOutput("git", []string{"remote", "get-url", remotes[0]})
+	} else {
+		fmt.Println("Failed getting remote setting.")
+		return ExitCodeError
+	}
+
+	remoteUrl, err := NewRemoteUrl(url)
+	if err != nil {
+		fmt.Println("No remote setting in this repository.")
+		return ExitCodeError
+	}
+
+	// gitlabじゃない場合
+	if !strings.HasPrefix(remoteUrl.Domain, "gitlab") {
+		fmt.Println("Not a cloned repository from gitlab. ", remoteUrl.Domain)
+		return ExitCodeError
+	}
+
+	browser := searchBrowserLauncher(runtime.GOOS)
+	cmdOutput(browser, []string{remoteUrl.ConcatUrl()})
+
+	return ExitCodeOK
+}
+
+type RemoteUrl struct {
+	Url        string
+	Domain     string
+	User       string
+	Repository string
+}
+
+func (r *RemoteUrl) ConcatUrl() string {
+	params := strings.Join([]string{r.Domain, r.User, r.Repository}, "/")
+	return "https://" + params
+}
+
+func NewRemoteUrl(url string) (*RemoteUrl, error) {
+	var (
+		otherScheme string
+		domain      string
+		user        string
+		repository  string
+	)
+
+	if strings.HasPrefix(url, "ssh") {
+		// ssh://git@gitlab.com/lighttiger2505/lab.git
+		otherScheme = strings.Split(url, "@")[1]
+		otherScheme = strings.TrimSuffix(otherScheme, ".git")
+	} else if strings.HasPrefix(url, "https") {
+		// https://github.com/lighttiger2505/lab
+		otherScheme = strings.Split(url, "//")[1]
+	} else {
+		return nil, errors.New(fmt.Sprintf("Invalid remote url: %s", url))
+	}
+
+	splitUrl := strings.Split(otherScheme, "/")
+	domain = splitUrl[0]
+	user = splitUrl[1]
+	repository = splitUrl[2]
+
+	return &RemoteUrl{
+		Url:        url,
+		Domain:     domain,
+		User:       user,
+		Repository: repository,
+	}, nil
+}
+
+func gitOutput(name string, args []string) string {
+	return gitOutputs(name, args)[0]
+}
+
+func gitOutputs(name string, args []string) []string {
+	var out = cmdOutput(name, args)
+	var outs []string
+	for _, line := range strings.Split(string(out), "\n") {
+		if strings.TrimSpace(line) != "" {
+			outs = append(outs, string(line))
+		}
+	}
+	return outs
+}
+
+func cmdOutput(name string, args []string) string {
+	out, err := exec.Command(name, args...).CombinedOutput()
+	if err != nil {
+		log.Println(err)
+	}
+	return string(out)
+}
+
+func searchBrowserLauncher(goos string) (browser string) {
+	switch goos {
+	case "darwin":
+		browser = "open"
+	case "windows":
+		browser = "cmd /c start"
+	default:
+		candidates := []string{
+			"xdg-open",
+			"cygstart",
+			"x-www-browser",
+			"firefox",
+			"opera",
+			"mozilla",
+			"netscape",
+		}
+		for _, b := range candidates {
+			path, err := exec.LookPath(b)
+			if err == nil {
+				browser = path
+				break
+			}
+		}
+	}
+	return browser
 }
 
 func main() {

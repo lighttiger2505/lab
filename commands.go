e2b581831882cdad35a0bd8de28328ba38af23f6
lighttiger <lighttiger2505@gmail.com>
2017-10-23 04:22:16 +0900

    Add func of create new config file

--- commands.go
+++ commands.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"flag"
 	"fmt"
+	"os"
 	"runtime"
 	"strings"
 
 	"github.com/mitchellh/cli"
+	"github.com/mitchellh/go-homedir"
 	"github.com/ryanuber/columnize"
 	"github.com/spf13/viper"
 	"github.com/xanzy/go-gitlab"
@@ -174,11 +177,18 @@ func ProjectId(client *gitlab.Client, gitlabRemote *GitRemote) (int, error) {
 
 func GitlabClient(gitlabRemote *GitRemote) (*gitlab.Client, error) {
 	// Read config file
-	viper.SetConfigName("config")
+	viper.SetConfigName(".labconfig")
 	viper.SetConfigType("yaml")
+	viper.AddConfigPath("$HOME")
 	viper.AddConfigPath("$HOME/.lab")
 	if err := viper.ReadInConfig(); err != nil {
-		return nil, errors.New(fmt.Sprintf("Failed read config file: %s", err.Error()))
+		if err := CreateConfig(); err != nil {
+			return nil, errors.New(fmt.Sprintf("Failed create config file: %s", err.Error()))
+		}
+
+		if err := viper.ReadInConfig(); err != nil {
+			return nil, errors.New(fmt.Sprintf("Failed read config file: %s", err.Error()))
+		}
 	}
 	privateToken := viper.GetString("private_token")
 
@@ -187,6 +197,29 @@ func GitlabClient(gitlabRemote *GitRemote) (*gitlab.Client, error) {
 	client.SetBaseURL(gitlabRemote.ApiUrl())
 
 	return client, nil
+}
+
+func CreateConfig() error {
+	dir, err := homedir.Dir()
+	if err != nil {
+		return errors.New(fmt.Sprintf("Failed get home dir: %s", err.Error()))
+	}
+
+	file, err := os.Create(fmt.Sprintf("%s/.labconfig.yml", dir))
+	if err != nil {
+		return errors.New(fmt.Sprintf("Failed create config file: %s", err.Error()))
+	}
+	defer file.Close()
+
+	fmt.Print("Plase input GitLab private token :")
+	stdin := bufio.NewScanner(os.Stdin)
+	stdin.Scan()
+	_, err = file.Write([]byte(fmt.Sprintf("private_token: %s", stdin.Text())))
+	if err != nil {
+		return errors.New(fmt.Sprintf("Failed write config file: %s", err.Error()))
+	}
+
+	return nil
 }
 
 type MergeRequestCommand struct {

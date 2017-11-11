dbb230cf5269600a1bdb5e6f8d5b00a309395910
lighttiger <lighttiger2505@gmail.com>
2017-11-12 04:40:12 +0900

    Fix search option default

--- commands.go
+++ commands.go
@@ -13,11 +13,11 @@ import (
 )
 
 type SearchOptons struct {
-	Line    int    `short:"n" long:"line" default-mask:"20" description:"output the NUM lines"`
-	State   string `short:"t" long:"state" default-mask:"all" description:"just those that are opened, closed or all"`
-	Scope   string `short:"c" long:"scope" default-mask:"all" description:"given scope: created-by-me, assigned-to-me or all."`
-	OrderBy string `short:"o" long:"orderby" default-mask:"updated_at" description:"ordered by created_at or updated_at fields."`
-	Sort    string `short:"s" long:"sort" default-mask:"desc" description:"sorted in asc or desc order."`
+	Line    int    `short:"n" long:"line" default:"20" default-mask:"20" description:"output the NUM lines"`
+	State   string `short:"t" long:"state" default:"all" default-mask:"all" description:"just those that are opened, closed or all"`
+	Scope   string `short:"c" long:"scope" default:"all" default-mask:"all" description:"given scope: created-by-me, assigned-to-me or all."`
+	OrderBy string `short:"o" long:"orderby" default:"updated_at" default-mask:"updated_at" description:"ordered by created_at or updated_at fields."`
+	Sort    string `short:"s" long:"sort" default:"desc" default-mask:"desc" description:"sorted in asc or desc order."`
 }
 
 var searchOptions SearchOptons

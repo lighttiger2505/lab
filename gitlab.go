fd9eb15bfb6a1e262eddf8a1887e69fbdd02d468
lighttiger <lighttiger2505@gmail.com>
2017-11-11 21:28:33 +0900

    Add func of choice url for multi gitlab remote url

--- gitlab.go
+++ gitlab.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"os"
+	"strconv"
 	"strings"
 
 	"github.com/xanzy/go-gitlab"
@@ -17,8 +20,30 @@ func FilterGitlabRemote(remoteInfos []RemoteInfo) (*RemoteInfo, error) {
 	}
 
 	var gitLabRemote RemoteInfo
-	if len(gitlabRemotes) > 0 {
+	if len(gitlabRemotes) == 1 {
 		gitLabRemote = gitlabRemotes[0]
+	} else if len(gitlabRemotes) > 1 {
+		fmt.Println("That repository existing multi gitlab remote url.")
+		for i, remoteInfo := range gitlabRemotes {
+			fmt.Println(fmt.Sprintf("%d) %s", i+1, remoteInfo.Domain))
+		}
+
+		fmt.Print(" Please choice target domain :")
+		stdin := bufio.NewScanner(os.Stdin)
+		stdin.Scan()
+		text := stdin.Text()
+
+		choiceNumber, err := strconv.Atoi(text)
+		if err != nil {
+			return nil, fmt.Errorf("Failed parse number. %v", err.Error())
+		}
+		if choiceNumber < 1 {
+			return nil, fmt.Errorf("Invalid numver. %d", choiceNumber)
+		} else if choiceNumber > len(gitlabRemotes) {
+			return nil, fmt.Errorf("Invalid numver. %d", choiceNumber)
+		}
+
+		gitLabRemote = gitlabRemotes[choiceNumber-1]
 	} else {
 		return nil, errors.New("Not a cloned repository from gitlab.")
 	}

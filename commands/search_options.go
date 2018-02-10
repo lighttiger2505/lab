08ee1b162db106ac86f373bddd88dd2cfd5d53d4
lighttiger <lighttiger2505@gmail.com>
2018-02-11 08:23:49 +0900

    Add show project all option on issue command
    GitHub Issue: #17

--- commands/search_options.go
+++ commands/search_options.go
@@ -8,9 +8,10 @@ var searchOptions SearchOptons
 var searchParser = flags.NewParser(&searchOptions, flags.Default)
 
 type SearchOptons struct {
-	Line    int    `short:"n" long:"line" default:"20" default-mask:"20" description:"output the NUM lines"`
-	State   string `short:"t" long:"state" default:"all" default-mask:"all" description:"just those that are opened, closed or all"`
-	Scope   string `short:"c" long:"scope" default:"all" default-mask:"all" description:"given scope: created-by-me, assigned-to-me or all."`
-	OrderBy string `short:"o" long:"orderby" default:"updated_at" default-mask:"updated_at" description:"ordered by created_at or updated_at fields."`
-	Sort    string `short:"s" long:"sort" default:"desc" default-mask:"desc" description:"sorted in asc or desc order."`
+	Line       int    `short:"n" long:"line" default:"20" default-mask:"20" description:"output the NUM lines"`
+	State      string `short:"t" long:"state" default:"all" default-mask:"all" description:"just those that are opened, closed or all"`
+	Scope      string `short:"c" long:"scope" default:"all" default-mask:"all" description:"given scope: created-by-me, assigned-to-me or all."`
+	OrderBy    string `short:"o" long:"orderby" default:"updated_at" default-mask:"updated_at" description:"ordered by created_at or updated_at fields."`
+	Sort       string `short:"s" long:"sort" default:"desc" default-mask:"desc" description:"sorted in asc or desc order."`
+	AllProject bool   `short:"a" long:"all-project" description:"output the NUM lines"`
 }

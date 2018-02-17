f709bac1069a23299494192a02e60489ee6dbc7f
lighttiger <lighttiger2505@gmail.com>
2018-02-17 09:05:50 +0900

    Move global option

--- commands/option.go
+++ commands/option.go
@@ -1,8 +1,18 @@
 package commands
 
 import (
+	"fmt"
+	"strings"
+
 	flags "github.com/jessevdk/go-flags"
 )
+
+var globalOpt GlobalOpt
+var globalParser = flags.NewParser(&globalOpt, flags.Default)
+
+type GlobalOpt struct {
+	Repository string `short:"p" long:"repository" description:"target specific repository"`
+}
 
 var searchOptions SearchOpt
 var searchParser = flags.NewParser(&searchOptions, flags.Default)
@@ -38,4 +48,13 @@ func (s *SearchOpt) GetScope() string {
 		return "assigned-to-me"
 	}
 	return s.Scope
+}
+
+func (g *GlobalOpt) ValidRepository() (string, string, error) {
+	value := g.Repository
+	splited := strings.Split(value, "/")
+	if value != "" && len(splited) != 2 {
+		return "", "", fmt.Errorf("Invalid repository \"%s\". Assumed input style is \"Namespace/Project\".", value)
+	}
+	return splited[0], splited[1], nil
 }

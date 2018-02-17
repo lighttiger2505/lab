80e14547583c4340c39f453f5518b0dcb7f8c486
lighttiger <lighttiger2505@gmail.com>
2018-02-17 09:12:01 +0900

    Fix test

--- commands/add_merge_request_test.go
+++ commands/add_merge_request_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestCreateMergeRequestMessage(t *testing.T) {
 	got := createMergeRequestMessage("title", "description")
-	want := `<!-- Write a message for this issue. The first block of text is the title -->
+	want := `<!-- Write a message for this merge request. The first block of text is the title -->
 title
 
 <!-- the rest is the description.  -->

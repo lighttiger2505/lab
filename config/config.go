88a2cab0aae615a8d1fc3112c6db1ad30bf0cc16
lighttiger <lighttiger2505@gmail.com>
2018-02-24 21:14:57 +0900

    Update config

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"syscall"
@@ -11,48 +12,153 @@ import (
 	"github.com/mitchellh/go-homedir"
 )
 
+var ConfigDataTest = `tokens:
+  gitlab.ssl.domain1.jp: token1
+  gitlab.ssl.domain2.jp: token2
+preferreddomains:
+- gitlab.ssl.domain1.jp
+- gitlab.ssl.domain2.jp
+`
+
+var TokensTest = yaml.MapSlice{
+	yaml.MapItem{
+		Key:   "gitlab.ssl.domain1.jp",
+		Value: "token1",
+	},
+	yaml.MapItem{
+		Key:   "gitlab.ssl.domain2.jp",
+		Value: "token2",
+	},
+}
+
+var PreferredDomainTest = []string{
+	"gitlab.ssl.domain1.jp",
+	"gitlab.ssl.domain2.jp",
+}
+
 type ConfigManager struct {
 	Path   string
+	File   *os.File
 	Config *Config
 }
 
 func NewConfigManager() *ConfigManager {
+	return NewConfigManagerPath("")
+}
+
+func NewConfigManagerPath(path string) *ConfigManager {
 	return &ConfigManager{
-		Path:   "",
+		Path:   path,
 		Config: nil,
 	}
 }
 
 func (c *ConfigManager) Init() error {
-	filepath := c.Path
-	if filepath == "" {
-		filepath := getConfigPath()
-		if !fileExists(filepath) {
-			if err := createConfig(filepath); err != nil {
-				return fmt.Errorf("Not exist config: %s", filepath)
-			}
+	if c.Path != "" {
+		return nil
+	}
+
+	path := getConfigPath()
+	if !fileExists(path) {
+		if err := MakeConfig(path); err != nil {
+			return fmt.Errorf("Not exist config: %s", path)
 		}
-		c.Path = filepath
+	}
+	c.Path = path
+
+	return nil
+}
+
+func (c *ConfigManager) InitPath(path string) {
+	c.Path = path
+}
+
+func (c *ConfigManager) Load() (conf *Config, err error) {
+	if c.Path == "" {
+		return nil, fmt.Errorf("Please initialize config manager")
+	}
+
+	file, err := c.open(os.O_RDONLY, 0666)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if cerr := c.close(); err != nil {
+			err = cerr
+		}
+	}()
+
+	conf, err = c.read(file)
+	if err != nil {
+		return nil, err
+	}
+	c.Config = conf
+	return
+}
+
+func (c *ConfigManager) open(flag int, perm os.FileMode) (*os.File, error) {
+	if !fileExists(c.Path) {
+		return nil, fmt.Errorf("Not exist config: Path %s", c.Path)
+	}
+
+	file, err := os.OpenFile(c.Path, flag, perm)
+	if err != nil {
+		return nil, fmt.Errorf("Filed open file. Error: %s", err.Error())
+	}
+	c.File = file
+	return file, nil
+}
+
+func (c *ConfigManager) read(r io.Reader) (*Config, error) {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, fmt.Errorf("Failed unmarshal yaml. Error: %s", err.Error())
+	}
+
+	conf := &Config{}
+	if err := yaml.Unmarshal(b, conf); err != nil {
+		return nil, fmt.Errorf("Failed unmarshal yaml. \nError: %s \nBuffer: %s", err.Error(), string(b))
+	}
+	return conf, nil
+}
+
+func (c *ConfigManager) close() error {
+	if err := c.File.Close(); err != nil {
+		return fmt.Errorf("Filed close file. Error: %s", err.Error())
 	}
 	return nil
 }
 
-func (c *ConfigManager) Load() (*Config, error) {
-	if !fileExists(c.Path) {
-		return nil, fmt.Errorf("Not exist config: %s", c.Path)
-	}
-
-	configData, err := ioutil.ReadFile(c.Path)
+func (c *ConfigManager) Save() error {
+	file, err := c.open(os.O_WRONLY, 0666)
 	if err != nil {
-		return nil, fmt.Errorf("Failed read config file: %s", err.Error())
+		return err
+	}
+	defer func() {
+		if cerr := c.close(); err != nil {
+			err = cerr
+		}
+	}()
+
+	if err = c.write(file); err != nil {
+		return err
 	}
 
-	conf := Config{}
-	if err := yaml.Unmarshal(configData, &conf); err != nil {
-		return nil, fmt.Errorf("Failed unmarshal yaml: %s", err.Error())
+	return nil
+}
+
+func (c *ConfigManager) write(writer io.Writer) error {
+	out, err := yaml.Marshal(c.Config)
+	if err != nil {
+		return fmt.Errorf("Failed marshal config. Error: %v", err.Error())
 	}
-	c.Config = &conf
-	return &conf, nil
+
+	fmt.Println("output-------------------------------------------")
+	fmt.Println(string(out))
+	if _, err = io.WriteString(writer, string(out)); err != nil {
+		return fmt.Errorf("Failed write config file. Error: %s", err.Error())
+	}
+	return nil
 }
 
 type Config struct {
@@ -63,7 +169,7 @@ type Config struct {
 func NewConfig() (*Config, error) {
 	filepath := getConfigPath()
 	if !fileExists(filepath) {
-		err := createConfig(filepath)
+		err := MakeConfig(filepath)
 		if err != nil {
 			return nil, fmt.Errorf("Not exist config: %s", filepath)
 		}
@@ -110,7 +216,7 @@ func fileExists(filename string) bool {
 	return true
 }
 
-func createConfig(filePath string) error {
+func MakeConfig(filePath string) error {
 	config := Config{}
 
 	file, err := os.Create(filePath)
@@ -151,6 +257,15 @@ func (c *Config) Write() error {
 	}
 
 	return nil
+}
+
+func (c *Config) GetToken(domain string) (token string) {
+	for _, mapItem := range c.Tokens {
+		if mapItem.Key.(string) == domain {
+			token = mapItem.Value.(string)
+		}
+	}
+	return
 }
 
 func (c *Config) AddToken(domain string, token string) {
--- config/config_test.go
+++ config/config_test.go
@@ -1,50 +1,128 @@
 package config
 
 import (
-	"fmt"
-
-	homedir "github.com/mitchellh/go-homedir"
-	// "reflect"
+	"bytes"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"strings"
 	"testing"
-	// "gopkg.in/yaml.v2"
 )
 
-// func TestConfig(t *testing.T) {
-// 	want := Config{
-// 		Tokens: &yaml.MapSlice{
-// 			yaml.MapItem{
-// 				Key:   "gitlab.ssl.iridge.jp",
-// 				Value: "SkLaKmzYDYVsD2bQ2TR",
-// 			},
-// 		},
-// 		Repositorys: []string{"gitlab.ssl.iridge.jp"},
-// 		Line:        30,
-// 		Scope:       "assigned-to-me",
-// 		State:       "closed",
-// 		Orderby:     "updated_at",
-// 		Sort:        "asc",
-// 	}
-// 	got, err := NewConfig()
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 		t.Fail()
-// 	}
+// var ConfigDataTest = `tokens:
+//   gitlab.ssl.domain1.jp: token1
+//   gitlab.ssl.domain2.jp: token2
+// preferreddomains:
+// - gitlab.ssl.domain1.jp
+// - gitlab.ssl.domain2.jp
+// `
 //
-// 	fmt.Println(fmt.Sprintf("%v", got))
-// 	if !reflect.DeepEqual(want, got) {
-// 		t.Errorf("bad return value want %#v got %#v", want, got)
-// 	}
+// var TokensTest = yaml.MapSlice{
+// 	yaml.MapItem{
+// 		Key:   "gitlab.ssl.domain1.jp",
+// 		Value: "token1",
+// 	},
+// 	yaml.MapItem{
+// 		Key:   "gitlab.ssl.domain2.jp",
+// 		Value: "token2",
+// 	},
+// }
+//
+// var PreferredDomainTest = []string{
+// 	"gitlab.ssl.domain1.jp",
+// 	"gitlab.ssl.domain2.jp",
 // }
 
-func TestConfig(t *testing.T) {
-	dir, err := homedir.Dir()
+func TestConfigManagerLoad(t *testing.T) {
+	f, _ := ioutil.TempFile("", "test")
+	tmppath := f.Name()
+	defer os.Remove(tmppath)
+
+	configData := ConfigDataTest
+	f.Write([]byte(configData))
+
+	conf := NewConfigManager()
+	conf.Path = tmppath
+	c, err := conf.Load()
 	if err != nil {
-		fmt.Println(err.Error())
+		t.Fatalf("wrong error. errors: \n%s", err.Error())
 	}
 
-	filePath := fmt.Sprintf("%s/labtest.yml", dir)
-	err1 := createConfig(filePath)
-	if err1 != nil {
-		fmt.Println(err1.Error())
+	wantTokens := TokensTest
+	wantPreferredDomains := PreferredDomainTest
+	if !reflect.DeepEqual(wantTokens, c.Tokens) || !reflect.DeepEqual(wantPreferredDomains, c.PreferredDomains) {
+		t.Fatalf("bad return value \nwant %#v %#v \ngot  %#v %#v", wantTokens, wantPreferredDomains, c.Tokens, c.PreferredDomains)
+	}
+}
+
+func TestConfigManagerRead(t *testing.T) {
+	configData := ConfigDataTest
+	r := strings.NewReader(configData)
+
+	conf := NewConfigManager()
+	c, err := conf.read(r)
+	if err != nil {
+		t.Fatalf("wrong error. errors: \n%s", err.Error())
+	}
+
+	wantTokens := TokensTest
+	wantPreferredDomains := PreferredDomainTest
+	if !reflect.DeepEqual(wantTokens, c.Tokens) || !reflect.DeepEqual(wantPreferredDomains, c.PreferredDomains) {
+		t.Fatalf("bad return value \nwant %#v %#v \ngot  %#v %#v", wantTokens, wantPreferredDomains, c.Tokens, c.PreferredDomains)
+	}
+}
+
+func TestConfigManagerSave(t *testing.T) {
+	f, _ := ioutil.TempFile("", "test")
+	tmppath := f.Name()
+	f.Close()
+	defer os.Remove(tmppath)
+
+	conf := NewConfigManagerPath(tmppath)
+	conf.Config = &Config{
+		Tokens:           TokensTest,
+		PreferredDomains: PreferredDomainTest,
+	}
+	if err := conf.Save(); err != nil {
+		t.Fatalf("wrong error. errors: \n%s", err.Error())
+	}
+
+	read, _ := ioutil.ReadFile(tmppath)
+	got := string(read)
+	want := ConfigDataTest
+	if want != got {
+		t.Fatalf("bad write value \nwant %s \ngot  %s", want, got)
+	}
+}
+
+func TestConfigManagerWrite(t *testing.T) {
+	w := bytes.NewBufferString("")
+	conf := NewConfigManager()
+	conf.Config = &Config{
+		Tokens:           TokensTest,
+		PreferredDomains: PreferredDomainTest,
+	}
+
+	err := conf.write(w)
+	if err != nil {
+		t.Fatalf("wrong error. errors: \n%s", err.Error())
+	}
+
+	want := ConfigDataTest
+	got := w.String()
+	if want != got {
+		t.Fatalf("bad return value \nwant %s \ngot  %s", want, got)
+	}
+}
+
+func TestConfigGetToken(t *testing.T) {
+	conf := &Config{
+		Tokens:           TokensTest,
+		PreferredDomains: PreferredDomainTest,
+	}
+	got := conf.GetToken("gitlab.ssl.domain1.jp")
+	want := "token1"
+	if want != got {
+		t.Fatalf("bad return value \nwant %s \ngot  %s", want, got)
 	}
 }

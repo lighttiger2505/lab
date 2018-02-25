95a6067fd6af4e86f2a4f03dc274c3ff8509c02f
lighttiger <lighttiger2505@gmail.com>
2018-02-26 00:20:37 +0900

    Clean without gitlab client process

--- gitlab/gitlab.go
+++ gitlab/gitlab.go
@@ -8,7 +8,6 @@ import (
 	"github.com/lighttiger2505/lab/config"
 	"github.com/lighttiger2505/lab/git"
 	"github.com/lighttiger2505/lab/ui"
-	"github.com/xanzy/go-gitlab"
 )
 
 type RemoteFilter interface {
@@ -109,109 +108,4 @@ func inputUseRemote(ui ui.Ui, remoteInfos []git.RemoteInfo) (*git.RemoteInfo, er
 
 	gitLabRemote := &remoteInfos[choiceNumber-1]
 	return gitLabRemote, nil
-}
-
-func NewGitlabClient(ui ui.Ui, gitlabRemote *git.RemoteInfo, token string) (*gitlab.Client, error) {
-	client := gitlab.NewClient(nil, token)
-	if err := client.SetBaseURL(gitlabRemote.ApiUrl()); err != nil {
-		return nil, fmt.Errorf("Invalid api url. %s", err.Error())
-	}
-	return client, nil
-}
-
-func ParceRepositoryFullName(webURL string) string {
-	sp := strings.Split(webURL, "/")
-	return strings.Join([]string{sp[3], sp[4]}, "/")
-}
-
-type Client interface {
-	Issues(baseurl, token string, opt *gitlab.ListIssuesOptions) ([]*gitlab.Issue, error)
-	ProjectIssues(baseurl, token string, opt *gitlab.ListProjectIssuesOptions, repositoryName string) ([]*gitlab.Issue, error)
-	MergeRequest(baseurl, token string, opt *gitlab.ListMergeRequestsOptions) ([]*gitlab.MergeRequest, error)
-	ProjectMergeRequest(baseurl, token string, opt *gitlab.ListProjectMergeRequestsOptions, repositoryName string) ([]*gitlab.MergeRequest, error)
-}
-
-type LabClient struct {
-	Client
-}
-
-func NewLabClient() *LabClient {
-	return &LabClient{}
-}
-
-func (g *LabClient) Issues(baseurl, token string, opt *gitlab.ListIssuesOptions) ([]*gitlab.Issue, error) {
-	client := gitlab.NewClient(nil, token)
-	if err := client.SetBaseURL(baseurl); err != nil {
-		return nil, fmt.Errorf("Invalid api url. %s", err.Error())
-	}
-
-	issues, _, err := client.Issues.ListIssues(opt)
-	if err != nil {
-		return nil, fmt.Errorf("Failed list issue. %s", err.Error())
-	}
-	return issues, nil
-}
-
-func (g *LabClient) ProjectIssues(baseurl, token string, opt *gitlab.ListProjectIssuesOptions, repositoryName string) ([]*gitlab.Issue, error) {
-	client := gitlab.NewClient(nil, token)
-	if err := client.SetBaseURL(baseurl); err != nil {
-		return nil, fmt.Errorf("Invalid api url. %s", err.Error())
-	}
-
-	issues, _, err := client.Issues.ListProjectIssues(repositoryName, opt)
-	if err != nil {
-		return nil, fmt.Errorf("Failed list project issue. %s", err.Error())
-	}
-	return issues, nil
-}
-
-func (g *LabClient) MergeRequest(baseurl, token string, opt *gitlab.ListMergeRequestsOptions) ([]*gitlab.MergeRequest, error) {
-	client := gitlab.NewClient(nil, token)
-	if err := client.SetBaseURL(baseurl); err != nil {
-		return nil, fmt.Errorf("Invalid api url. %s", err.Error())
-	}
-
-	mergeRequests, _, err := client.MergeRequests.ListMergeRequests(opt)
-	if err != nil {
-		return nil, fmt.Errorf("Failed list merge requests. %s", err.Error())
-	}
-
-	return mergeRequests, nil
-}
-
-func (g *LabClient) ProjectMergeRequest(baseurl, token string, opt *gitlab.ListProjectMergeRequestsOptions, repositoryName string) ([]*gitlab.MergeRequest, error) {
-	client := gitlab.NewClient(nil, token)
-	if err := client.SetBaseURL(baseurl); err != nil {
-		return nil, fmt.Errorf("Invalid api url. %s", err.Error())
-	}
-
-	mergeRequests, _, err := client.MergeRequests.ListProjectMergeRequests(repositoryName, opt)
-	if err != nil {
-		return nil, fmt.Errorf("Failed list project merge requests. %s", err.Error())
-	}
-
-	return mergeRequests, nil
-}
-
-type MockLabClient struct {
-	MockIssues              func(baseurl, token string, opt *gitlab.ListIssuesOptions) ([]*gitlab.Issue, error)
-	MockProjectIssues       func(baseurl, token string, opt *gitlab.ListProjectIssuesOptions, repositoryName string) ([]*gitlab.Issue, error)
-	MockMergeRequest        func(baseurl, token string, opt *gitlab.ListMergeRequestsOptions) ([]*gitlab.MergeRequest, error)
-	MockProjectMergeRequest func(baseurl, token string, opt *gitlab.ListProjectMergeRequestsOptions, repositoryName string) ([]*gitlab.MergeRequest, error)
-}
-
-func (m *MockLabClient) Issues(baseurl, token string, opt *gitlab.ListIssuesOptions) ([]*gitlab.Issue, error) {
-	return m.MockIssues(baseurl, token, opt)
-}
-
-func (m *MockLabClient) ProjectIssues(baseurl, token string, opt *gitlab.ListProjectIssuesOptions, repositoryName string) ([]*gitlab.Issue, error) {
-	return m.MockProjectIssues(baseurl, token, opt, repositoryName)
-}
-
-func (m *MockLabClient) MergeRequest(baseurl, token string, opt *gitlab.ListMergeRequestsOptions) ([]*gitlab.MergeRequest, error) {
-	return m.MockMergeRequest(baseurl, token, opt)
-}
-
-func (m *MockLabClient) ProjectMergeRequest(baseurl, token string, opt *gitlab.ListProjectMergeRequestsOptions, repositoryName string) ([]*gitlab.MergeRequest, error) {
-	return m.MockProjectMergeRequest(baseurl, token, opt, repositoryName)
 }

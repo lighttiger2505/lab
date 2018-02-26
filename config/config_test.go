50f6d6ddac700790ed3d6dd80a08a92fa40cb18e
lighttiger <lighttiger2505@gmail.com>
2018-02-27 00:09:54 +0900

    Add test

--- config/config_test.go
+++ config/config_test.go
@@ -7,6 +7,8 @@ import (
 	"reflect"
 	"strings"
 	"testing"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
 func TestConfigManagerLoad(t *testing.T) {
@@ -100,5 +102,78 @@ func TestConfigGetToken(t *testing.T) {
 	want := "token1"
 	if want != got {
 		t.Fatalf("bad return value \nwant %s \ngot  %s", want, got)
+	}
+}
+
+func TestConfigHasDomain(t *testing.T) {
+	conf := &Config{
+		Tokens:           TokensTest,
+		PreferredDomains: PreferredDomainTest,
+	}
+	got := conf.hasDomain("gitlab.ssl.domain2.jp")
+	want := true
+	if want != got {
+		t.Fatalf("bad return value \nwant %v \ngot  %v", want, got)
+	}
+
+	got = conf.hasDomain("unknown")
+	want = false
+	if want != got {
+		t.Fatalf("bad return value \nwant %v \ngot  %v", want, got)
+	}
+}
+
+func TestConfigGetTopDomain(t *testing.T) {
+	conf := &Config{
+		Tokens:           TokensTest,
+		PreferredDomains: PreferredDomainTest,
+	}
+	got := conf.getTopDomain()
+	want := "gitlab.ssl.domain1.jp"
+	if want != got {
+		t.Fatalf("bad return value \nwant %s \ngot  %s", want, got)
+	}
+}
+
+func TestConfigAddToken(t *testing.T) {
+	conf := &Config{
+		Tokens:           TokensTest,
+		PreferredDomains: PreferredDomainTest,
+	}
+	conf.addToken("NewToken", "NewDomain")
+	got := conf.Tokens
+	want := yaml.MapSlice{
+		yaml.MapItem{
+			Key:   "gitlab.ssl.domain1.jp",
+			Value: "token1",
+		},
+		yaml.MapItem{
+			Key:   "gitlab.ssl.domain2.jp",
+			Value: "token2",
+		},
+		yaml.MapItem{
+			Key:   "NewDomain",
+			Value: "NewToken",
+		},
+	}
+	if reflect.DeepEqual(want, got) {
+		t.Fatalf("bad return value \nwant %v \ngot  %v", want, got)
+	}
+}
+
+func TestConfigAddRepository(t *testing.T) {
+
+	conf := &Config{
+		Tokens:           TokensTest,
+		PreferredDomains: PreferredDomainTest,
+	}
+	conf.AddRepository("NewDomain")
+	got := conf.PreferredDomains
+	want := []string{
+		"gitlab.ssl.domain1.jp",
+		"gitlab.ssl.domain2.jp",
+	}
+	if reflect.DeepEqual(want, got) {
+		t.Fatalf("bad return value \nwant %v \ngot  %v", want, got)
 	}
 }

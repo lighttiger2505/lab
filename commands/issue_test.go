1f0a471c946c4530ae968aea017ce87d7a8a2b95
lighttiger2505 <lighttiger2505@gmail.com>
2018-03-19 03:35:42 +0900

    Add show issue test

--- commands/issue_test.go
+++ commands/issue_test.go
@@ -3,28 +3,63 @@ package commands
 import (
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/lighttiger2505/lab/git"
-	"github.com/lighttiger2505/lab/gitlab"
+	lab "github.com/lighttiger2505/lab/gitlab"
 	"github.com/lighttiger2505/lab/ui"
-	gitlabc "github.com/xanzy/go-gitlab"
+	gitlab "github.com/xanzy/go-gitlab"
 )
 
-var issues = []*gitlabc.Issue{
-	&gitlabc.Issue{IID: 12, Title: "Title12", WebURL: "http://gitlab.jp/namespace/repo12"},
-	&gitlabc.Issue{IID: 13, Title: "Title13", WebURL: "http://gitlab.jp/namespace/repo13"},
+var createdAt, _ = time.Parse("2006-01-02", "2018-02-14")
+var updatedAt, _ = time.Parse("2006-01-02", "2018-03-14")
+
+var issue = &gitlab.Issue{
+	IID:   12,
+	Title: "Title12",
+	Assignee: struct {
+		ID        int    `json:"id"`
+		Name      string `json:"name"`
+		Username  string `json:"username"`
+		State     string `json:"state"`
+		AvatarURL string `json:"avatar_url"`
+		WebURL    string `json:"web_url"`
+	}{
+		Name: "AssigneeName",
+	},
+	Author: struct {
+		ID        int        `json:"id"`
+		Username  string     `json:"username"`
+		Email     string     `json:"email"`
+		Name      string     `json:"name"`
+		State     string     `json:"state"`
+		CreatedAt *time.Time `json:"created_at"`
+	}{
+		Name: "AuthorName",
+	},
+	CreatedAt:   &createdAt,
+	UpdatedAt:   &updatedAt,
+	Description: "Description",
 }
 
-var mockGitlabIssueClient = &gitlab.MockLabClient{
-	MockIssues: func(opt *gitlabc.ListIssuesOptions) ([]*gitlabc.Issue, error) {
+var issues = []*gitlab.Issue{
+	&gitlab.Issue{IID: 12, Title: "Title12", WebURL: "http://gitlab.jp/namespace/repo12"},
+	&gitlab.Issue{IID: 13, Title: "Title13", WebURL: "http://gitlab.jp/namespace/repo13"},
+}
+
+var mockGitlabIssueClient = &lab.MockLabClient{
+	MockGetIssue: func(pid int, repositoryName string) (*gitlab.Issue, error) {
+		return issue, nil
+	},
+	MockIssues: func(opt *gitlab.ListIssuesOptions) ([]*gitlab.Issue, error) {
 		return issues, nil
 	},
-	MockProjectIssues: func(opt *gitlabc.ListProjectIssuesOptions, repositoryName string) ([]*gitlabc.Issue, error) {
+	MockProjectIssues: func(opt *gitlab.ListProjectIssuesOptions, repositoryName string) ([]*gitlab.Issue, error) {
 		return issues, nil
 	},
 }
 
-var mockIssueProvider = &gitlab.MockProvider{
+var mockIssueProvider = &lab.MockProvider{
 	MockInit: func() error { return nil },
 	MockGetSpecificRemote: func(namespace, project string) *git.RemoteInfo {
 		return &git.RemoteInfo{
@@ -40,12 +75,40 @@ var mockIssueProvider = &gitlab.MockProvider{
 			Repository: "repository",
 		}, nil
 	},
-	MockGetClient: func(remote *git.RemoteInfo) (gitlab.Client, error) {
+	MockGetClient: func(remote *git.RemoteInfo) (lab.Client, error) {
 		return mockGitlabIssueClient, nil
 	},
 }
 
-func TestIssueCommandRun_Issue(t *testing.T) {
+func TestIssueCommandRun_ShowIssue(t *testing.T) {
+	mockUI := ui.NewMockUi()
+	c := IssueCommand{
+		Ui:       mockUI,
+		Provider: mockIssueProvider,
+	}
+
+	args := []string{"12"}
+	if code := c.Run(args); code != 0 {
+		t.Fatalf("wrong exit code. errors: \n%s", mockUI.ErrorWriter.String())
+	}
+
+	got := mockUI.Writer.String()
+	want := `#12
+Title: Title12
+Assignee: AssigneeName
+Author: AuthorName
+CreatedAt: 2018-02-14 00:00:00 +0000 UTC
+UpdatedAt: 2018-03-14 00:00:00 +0000 UTC
+
+Description
+`
+
+	if got != want {
+		t.Fatalf("bad output value \nwant %s \ngot  %s", got, want)
+	}
+}
+
+func TestIssueCommandRun_ListIssue(t *testing.T) {
 	mockUI := ui.NewMockUi()
 	c := IssueCommand{
 		Ui:       mockUI,
@@ -65,7 +128,7 @@ func TestIssueCommandRun_Issue(t *testing.T) {
 	}
 }
 
-func TestIssueCommandRun_ProjectIssue(t *testing.T) {
+func TestIssueCommandRun_ListProjectIssue(t *testing.T) {
 	mockUI := ui.NewMockUi()
 	c := IssueCommand{
 		Ui:       mockUI,

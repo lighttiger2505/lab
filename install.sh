a53eef0a96c81ea2fa52c18539e199c062ecda4a
toshikazu.ohashi <toshikazu.ohashi@iridge.jp>
2018-04-20 20:00:27 +0900

    Fix install script

--- install.sh
+++ install.sh
@@ -23,6 +23,6 @@ esac
 
 github_url="https://github.com/lighttiger2505/lab"
 latest=$(curl -sL "${github_url}/tags" | grep tag-name | grep --only '>v[0-9\.]\+<' | head -n1 | cut -c 3- | rev | cut -c 2- | rev)
-curl -OL "${github_url}/releases/download/v${latest}/lab-v${latest}-${os}-${machine}.tar.gz" | tar -C /tmp/ -xzf -
+curl -sL "${github_url}/releases/download/v${latest}/lab-v${latest}-${os}-${machine}.tar.gz" | tar -C /tmp/ -xzf -
 cp /tmp/${os}-${machine}/lab /usr/local/bin/lab
 echo "Successfully installed lab into /usr/local/bin/"

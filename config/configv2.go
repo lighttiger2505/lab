ee8293b0bcd8216804f48f269e568c6f556ed59d
lighttiger2505 <lighttiger2505@gmail.com>
2018-11-28 20:14:09 +0900

    Add new config

---
+++ config/configv2.go
@@ -0,0 +1,134 @@
+package config
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type ConfigV2 struct {
+	Profiles       map[string]Profile `yaml:"profiles"`
+	DefalutProfile string             `yaml:"default_profile"`
+}
+
+type Profile struct {
+	Token          string `yaml:"token"`
+	DefaultGroup   string `yaml:"default_group"`
+	DefaultProject string `yaml:"default_project"`
+}
+
+func NewConfig() *ConfigV2 {
+	cfg := &ConfigV2{
+		Profiles: map[string]Profile{},
+	}
+	return cfg
+}
+
+func GetConfig() (*ConfigV2, error) {
+	cfg := NewConfig()
+	if err := cfg.Load(); err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
+
+func (c *ConfigV2) Load() error {
+	fPath := getXDGConfigPath()
+	if !fileExists(fPath) {
+		_, err := os.Create(fPath)
+		if err != nil {
+			return fmt.Errorf("cannot create config, %s", err.Error())
+		}
+	}
+	file, err := os.OpenFile(fPath, os.O_RDONLY, 0666)
+	if err != nil {
+		return fmt.Errorf("cannot open config, %s", err)
+	}
+	defer file.Close()
+
+	b, err := ioutil.ReadAll(file)
+	if err != nil {
+		return fmt.Errorf("cannot read config, %s", err)
+	}
+
+	if err = yaml.Unmarshal(b, c); err != nil {
+		return fmt.Errorf("failed unmarshal yaml. \nError: %s \nBuffer: %s", err, string(b))
+	}
+	return nil
+}
+
+func (c *ConfigV2) Save() error {
+	file, err := os.OpenFile(getXDGConfigPath(), os.O_WRONLY, 0666)
+	if err != nil {
+		return fmt.Errorf("cannot open file, %s", err)
+	}
+	defer file.Close()
+
+	out, err := yaml.Marshal(c)
+	if err != nil {
+		return fmt.Errorf("Failed marshal config. Error: %v", err)
+	}
+
+	if _, err = io.WriteString(file, string(out)); err != nil {
+		return fmt.Errorf("Failed write config file. Error: %s", err)
+	}
+	return nil
+}
+
+func (c *ConfigV2) GetProfile(domain string) (*Profile, error) {
+	profile, ok := c.Profiles[domain]
+	if !ok {
+		return nil, fmt.Errorf("Not found profile, %s. Please check config.", domain)
+	}
+	return &profile, nil
+}
+
+func (c *ConfigV2) GetDefaultProfile() *Profile {
+	profile, _ := c.GetProfile(c.DefalutProfile)
+	return profile
+}
+
+func (c *ConfigV2) SetProfile(domain string, profile Profile) {
+	c.Profiles[domain] = profile
+}
+
+func (c *ConfigV2) HasDomain(domain string) bool {
+	_, ok := c.Profiles[domain]
+	if !ok {
+		return false
+	}
+	return true
+}
+
+func (c *ConfigV2) GetToken(domain string) string {
+	profile, _ := c.GetProfile(domain)
+	return profile.Token
+}
+
+func (c *ConfigV2) SetToken(domain, token string) {
+	profile, _ := c.GetProfile(domain)
+	profile.Token = token
+	c.SetProfile(domain, *profile)
+}
+
+func getXDGConfigPath() string {
+	var dir string
+	if runtime.GOOS == "windows" {
+		dir = os.Getenv("APPDATA")
+		if dir == "" {
+			dir = filepath.Join(os.Getenv("USERPROFILE"), "Application Data", "lab")
+		}
+		dir = filepath.Join(dir, "lab")
+	} else {
+		dir = filepath.Join(os.Getenv("HOME"), ".config", "lab")
+	}
+	if err := os.MkdirAll(dir, 0700); err != nil {
+		panic(fmt.Sprintf("cannot create directory, %s", err))
+	}
+	return filepath.Join(dir, "config.yml")
+}
